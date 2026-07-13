0183fc5614abfcbd0e04f219f3566a4a22227158
beingkashifali <beingkashifali@gmail.com>
2026-07-13 21:22:35 +0500

    updated url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const contactRouter = require("./routes/contact");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://whoiskashifali.netlify.app/"],
   }),
 );
 app.use(express.json());
