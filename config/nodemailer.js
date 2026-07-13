a73fe72b3b4f13615db1319497e28a8f9997f82b
beingkashifali <beingkashifali@gmail.com>
2026-07-13 21:48:30 +0500

    bug fixes

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,22 +1,41 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-// Using Gmail to send emails
+// // Using Gmail to send emails
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_PASS,
+//   },
+// });
+
+// // Using external library for sending emails like brevo
+// // const transporter = nodemailer.createTransport({
+// //   host: "smtp-relay.brevo.com",
+// //   port: 587,
+// //   auth: {
+// //     user: process.env.SMTP_USER,
+// //     pass: process.env.SMTP_PASS,
+// //   },
+// // });
+
+// module.exports = transporter;
+
+const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// Prefer IPv4 addresses
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
 });
-
-// Using external library for sending emails like brevo
-// const transporter = nodemailer.createTransport({
-//   host: "smtp-relay.brevo.com",
-//   port: 587,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS,
-//   },
-// });
 
 module.exports = transporter;
